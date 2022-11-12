2e9047cae9b4e21e312c3db700e82a1c21ff940a
Ian1998Otuoma <115020729+Ian1998Otuoma@users.noreply.github.com>
2022-11-12 17:35:50 +0300

    Add files via upload

---
+++ area of rectangle .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float base,height,hypotenuse,perimeter;
+    //input
+    cout<<"enter the height of triangle.";
+    cin>>height;
+    cout<<"enter the base of the triangle";
+    cin>>base;
+    cout<<"enter the hypotenuse ot the triangle";
+    cin>>hypotenuse;
+    //calculating the area of the triangle
+    area=1/2*10*7
+    //display the results
+    cout<<"area and perimeter of the right angled triangle is"<<endl;
+}
